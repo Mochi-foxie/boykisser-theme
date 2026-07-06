2ddd18027257b4897dcf7bb7b03202af150bd0eb
Mochi <theheavenlyvanguard@gmail.com>
2026-07-06 07:23:59 -0400

    Update boykisser-plugin.plugin.js

--- boykisser-plugin.plugin.js
+++ boykisser-plugin.plugin.js
@@ -1,1 +1,21 @@
+//META{"name":"Boykisser Extra","author":"Mochi","version":"1.0","description":"Extra JS for Boykisser theme"}*//
 
+class BoykisserExtra {
+    start() {
+        console.log("%cBoykisser Extra Plugin Loaded", "color: #ff0066; font-size: 16px; font-weight: bold");
+        
+        // Example: Add extra lewd effects
+        this.addExtraDrips();
+    }
+
+    addExtraDrips() {
+        // You can add custom JS logic here later
+        console.log("Extra cum drips enabled 💦");
+    }
+
+    stop() {
+        console.log("Boykisser Extra Plugin Stopped");
+    }
+}
+
+module.exports = BoykisserExtra;
